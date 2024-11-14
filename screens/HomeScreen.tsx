fcd4d7d66de34bee894aaa57463084d9a63f860f
isabellaes <borjessonisabella@gmail.com>
2024-11-14 11:23:53 +0100

    add style to home page and navigation to details page

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,15 +1,54 @@
-import { ScrollView, View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import ProductCard from "../components/ProductCard";
 import { products } from "../utils/data";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/RootNavigator";
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Home"
+>;
 
 const HomeScreen = () => {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+
   return (
-    <ScrollView>
-      {products.map((p) => (
-        <ProductCard product={p} />
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.cardContainer}>
+            <ProductCard
+              product={item}
+              onPress={() => {
+                navigation.navigate("Product", { productId: item.id });
+              }}
+            />
+          </View>
+        )}
+        numColumns={2}
+        contentContainerStyle={styles.listContent}
+      />
+    </View>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#8874a3",
+  },
+  listContent: {
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+  },
+  cardContainer: {
+    flex: 1,
+    margin: 5,
+    maxWidth: "50%",
+  },
+});
